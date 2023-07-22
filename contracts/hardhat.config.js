b57b4aaf24cd1a521f5dc61ee150c428db1bab4e
LeNei <leonmarc.neisskenwirth@gmail.com>
2023-07-23 01:39:19 +0200

    update

--- contracts/hardhat.config.js
+++ contracts/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 // Go to https://infura.io, sign up, create a new API key
 // in its dashboard, and replace "KEY" with it
-const INFURA_API_KEY = "54b3b84d91de4ca6874203e3b1ab6f27";
+const INFURA_API_KEY = process.env.INFURA_API_KEY;
 
 // Replace this private key with your Sepolia account private key
 // To export your private key from Coinbase Wallet, go to
@@ -11,7 +11,7 @@ const INFURA_API_KEY = "54b3b84d91de4ca6874203e3b1ab6f27";
 // To export your private key from Metamask, open Metamask and
 // go to Account Details > Export Private Key
 // Beware: NEVER put real Ether into testing accounts
-const SEPOLIA_PRIVATE_KEY = "aaafafb4b95a536bd1a11f784a6ecc7ba6eec075544c75bb95a41c8fec132f40";
+const SEPOLIA_PRIVATE_KEY = process.env.SEPOLIA_PRIVATE_KEY;
 
 module.exports = {
   solidity: "0.8.19",
@@ -20,16 +20,16 @@ module.exports = {
       url: `https://sepolia.infura.io/v3/${INFURA_API_KEY}`,
       accounts: [SEPOLIA_PRIVATE_KEY]
     },
-    
+
     alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
       accounts: [SEPOLIA_PRIVATE_KEY],
-     chainId: 44787
-   },
+      chainId: 44787
+    },
     neonevm: {
       url: "https://devnet.neonscan.org",
       accounts: [SEPOLIA_PRIVATE_KEY],
       chainId: 245022926,
-      },
-    }
-  };
+    },
+  }
+};
